6036a2e5e0013519d8de3e6f52601f5b204b8b58
Ethan Montclair <montclair.ethan@gmail.com>
2020-07-15 00:15:29 -0600

    probwithconst

--- Part2/index.js
+++ Part2/index.js
@@ -1,1 +1,10 @@
-const something = document.getElementById('idInput')
+const idInput = document.getElementById('idInput');
+const colorInput = document.getElementById('colorInput');
+
+console.log(idInput);
+console.log(colorInput);
+
+function setCard(){
+    const card = document.getElementById(idInput.value)
+    console.log(card)
+}
